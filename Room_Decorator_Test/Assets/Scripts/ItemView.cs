7ed2084594123a31fe4f3fb2011c94ff6e2afc4e
GD_Zero <95521692+GD-Zero@users.noreply.github.com>
2022-01-29 11:59:18 +0300

    In development.
    Project in development.

---
+++ Room_Decorator_Test/Assets/Scripts/ItemView.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class ItemView : MonoBehaviour
+{
+    [SerializeField] private Image _image;
+    [SerializeField] private TMP_Text _label;
+    [SerializeField] private Button _selectionButton;
+
+    private ItemData _itemData;
+
+    public event UnityAction<ItemData> ItemSelected;
+    public event UnityAction<ItemView> ItemDisabled;
+
+    private void OnEnable()
+    {
+        _selectionButton.onClick.AddListener(OnSelectionButtonClick);
+    }
+
+    private void OnDisable()
+    {
+        ItemDisabled?.Invoke(this);
+        _selectionButton.onClick.RemoveListener(OnSelectionButtonClick);
+    }
+
+    private void OnSelectionButtonClick()
+    {
+        ItemSelected?.Invoke(_itemData);
+    }
+
+    public void Initialize(ItemData itemData)
+    {
+        _itemData = itemData;
+        _image.sprite = itemData.Preview;
+        _label.text = itemData.Label; 
+    }
+}
